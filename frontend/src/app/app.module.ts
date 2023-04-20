8585f56f86b09646358b088b77691ce445b23b39
Darya V <57266356+DaryaVkh@users.noreply.github.com>
2023-04-21 02:46:17 +0500

    Task 4

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TuiRootModule } from '@taiga-ui/core';
 import { AppComponent } from './app.component';
 import { CardListModule } from './components/card-list/card-list.module';
 
@@ -13,7 +12,6 @@ import { CardListModule } from './components/card-list/card-list.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    TuiRootModule,
     CardListModule,
     HttpClientModule
   ],
